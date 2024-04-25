c622fc472c428ec894a478d412ceeb728d0816de
Maurice Yang <mauriceyangmy28@gmail.com>
2024-04-25 18:52:05 +0700

    feat: Adding icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "react-toastify/dist/ReactToastify.css"
-  
+
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import { ToastContainer } from 'react-toastify';
@@ -13,6 +13,8 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
     <Head>
         <title>Heymart C14</title>
+        <link rel="icon" href="/favicon.ico" sizes="any"/>
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
     </Head>
     <div className={`font-sans ${inter.className}`}>
       <Component {...pageProps} />
