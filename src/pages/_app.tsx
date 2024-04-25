a7e2cc2eae77eb7a198502cb2903346d5e3cb187
Maurice Yang <mauriceyangmy28@gmail.com>
2024-04-25 13:08:35 +0700

    Inital commit + login page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { Inter } from "next/font/google";
+import Head from "next/head";
+
+const inter = Inter({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <Head>
+        <title>Heymart C14</title>
+    </Head>
+    <div className={`font-sans ${inter.className}`}>
+      <Component {...pageProps} />
+    </div>
+    </>
+)
+  ;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,17 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [{
+      mytheme: {
+        "primary": "#4a90e2",
+        "secondary": "#f6d860",
+        "accent": "#37cdbe",
+        "neutral": "#3d4451",
+        "base-100": "#ffffff",
+      }
+    }, "light"]
+  }
 };
 export default config;
