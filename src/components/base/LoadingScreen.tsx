26a91d6208c9c64c413bc9bf57c8df9b35ebea33
Maurice Yang <mauriceyangmy28@gmail.com>
2024-05-26 16:48:47 +0700

    feat: Making it prettier

--- src/components/base/LoadingScreen.tsx
+++ src/components/base/LoadingScreen.tsx
@@ -3,10 +3,10 @@ import Image from "next/image"
 const LoadingScreen = () => {
   return (
     <main className="w-full h-screen relative">
-        <Image src="bg.svg" alt="Heymart" fill className="object-cover block z-0" />
-        <div className="z-20 flex items-center justify-center gap-4 flex-col text-white">
-            <Image src="/icon-192.png" alt="Heymart" width={192} height={192} className=""/>
-            <h1 className="text-2xl">Loading...</h1>
+        <Image src="/bg.svg" alt="Heymart" fill className="object-cover block z-0" />
+        <div className="z-20 flex items-center justify-center gap-4 flex-col text-white w-full h-screen">
+            <Image src="/icon-192.png" alt="Heymart" width={150} height={150} className="z-10"/>
+            <h1 className="text-2xl sm:text-3xl font-semibold z-10">Loading...</h1>
         </div>
     </main>
   )
