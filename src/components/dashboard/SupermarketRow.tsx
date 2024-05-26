b4a3c1ac758a33f9050f62a9022ab7a9ef35dabf
juankhusuma <66712975+juankhusuma@users.noreply.github.com>
2024-05-26 17:00:55 +0700

    Finished CRUD supermarket

--- src/components/dashboard/SupermarketRow.tsx
+++ src/components/dashboard/SupermarketRow.tsx
@@ -1,13 +1,14 @@
-import { CheckCircleIcon, MoreHorizontal } from "lucide-react";
+import { CheckCheck, CheckCircleIcon, CircleX, MoreHorizontal } from "lucide-react";
 import { Button } from "../ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "../ui/dropdown-menu";
 import { TableCell, TableRow } from "../ui/table";
 import { toRupiah } from "@/util/rupiahFormater";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { SupermarketContext } from "@/pages/admin/dashboard";
 import { toast } from "sonner";
 import axios from "axios";
 import { useAuth } from "@/context/AuthContext";
+import { Input } from "../ui/input";
 
 interface SupermarketRowProps {
     id: number;
@@ -19,13 +20,47 @@ interface SupermarketRowProps {
 export default function SupermarketRow({ id, name, balance }: SupermarketRowProps) {
     const { supermarkets, setSupermarkets } = useContext(SupermarketContext)
     const { authToken } = useAuth();
+    const [edit, setEdit] = useState(false)
+    const [itemName, setItemName] = useState(name)
+
     return (
         <TableRow>
             <TableCell className="font-medium">
-                {name}
+                {!edit ? itemName : (
+                    <Input
+                        onChange={(e) => setItemName(e.target.value)}
+                        value={itemName}
+                    />
+                )}
             </TableCell>
             <TableCell>{toRupiah(balance)}</TableCell>
-            <TableCell>
+            {edit && <>
+                <TableCell>
+                    <CheckCheck className="cursor-pointer" onClick={async () => {
+                        toast("Supermarket updated successfully", {
+                            icon: <CheckCheck size={18} color="green" />
+                        })
+                        setEdit(false);
+                        await axios.put(`https://heymart-management-6ndsbjpnka-ew.a.run.app/supermarkets/${id}`, {
+                            name: itemName
+                        }, {
+                            headers: {
+                                Authorization: `Bearer ${authToken}`
+                            }
+                        })
+                    }} size={18} color="green" />
+                </TableCell>
+                <TableCell>
+                    <CircleX className="cursor-pointer" onClick={() => {
+                        setItemName(name)
+                        setEdit(false)
+                        toast("Edit canceled", {
+                            icon: <CheckCheck size={18} color="green" />
+                        })
+                    }} size={18} color="red" />
+                </TableCell>
+            </>}
+            {!edit && <TableCell>
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
                         <Button
@@ -39,7 +74,7 @@ export default function SupermarketRow({ id, name, balance }: SupermarketRowProp
                     </DropdownMenuTrigger>
                     <DropdownMenuContent align="end">
                         <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setEdit(true)}>Edit</DropdownMenuItem>
                         <DropdownMenuItem onClick={async () => {
                             await axios.delete(`https://heymart-management-6ndsbjpnka-ew.a.run.app/supermarkets/${id}`, {
                                 headers: {
@@ -53,7 +88,7 @@ export default function SupermarketRow({ id, name, balance }: SupermarketRowProp
                         }}>Delete</DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
-            </TableCell>
+            </TableCell>}
         </TableRow>
     )
 }
