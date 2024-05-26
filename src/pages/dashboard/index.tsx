173f71e86c94f7dcaf8943931dde3487391a5e06
Maurice Yang <mauriceyangmy28@gmail.com>
2024-05-26 17:05:34 +0700

    feat: Adding shopping cart

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -24,6 +24,7 @@ const OrdersPage = () => {
     const { authToken, user, setUser } = useAuth();
 
     const [orders, setOrders] = useState<Order[]>([]);
+    const [cart, setCart] = useState<Order[]>([]);
 
     const getOrders = async () => {
         try {
@@ -34,8 +35,9 @@ const OrdersPage = () => {
                     'Authorization': `Bearer ${authToken}`
                 }
             })
-            const data = await response.data
+            const data: Order[] = await response.data
             setOrders(data);
+            setCart(data.filter(order => order.status === "WaitingPayment"));
         } catch (error) {
             console.log(error)
         }
@@ -56,6 +58,13 @@ const OrdersPage = () => {
             setUser({
                 ...user,
                 balance: user.balance - data.subtotal
+            });
+            await axios.post(`${process.env.NEXT_PUBLIC_SUPERMARKET_URL}/supermarkets/topup/${data.supermarket_id}`, {
+                amount: data.subtotal
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
             });
                     toast.success("Payment Successfull", {
                         icon: <CheckCheck size={18} color="green" />,
@@ -117,7 +126,7 @@ const OrdersPage = () => {
                     <ShoppingCart className="h-10 w-10 text-primary" />
                     <div>
                         <h4 className="text-xl font-semibold">Wishlisted Items</h4>
-                        <p className="text-lg text-muted-foreground">You have 3 items in your cart</p>
+                        <p className="text-lg text-muted-foreground">You have {cart.length} items in your cart</p>
                     </div>
                 </div>
             </CardHeader>
@@ -136,6 +145,7 @@ const OrdersPage = () => {
                             <TableRow>
                                 <TableHead>Order ID</TableHead>
                                 <TableHead>Product Name</TableHead>
+                                <TableHead>Quantity</TableHead>
                                 <TableHead>Subtotal</TableHead>
                                 <TableHead className="hidden md:table-cell">
                                     Status
@@ -158,6 +168,9 @@ const OrdersPage = () => {
             </TableCell>
             <TableCell className="font-medium">
                 {order.product_name}
+            </TableCell>
+            <TableCell className="font-medium">
+                {order.quantity}
             </TableCell>
             <TableCell>{toRupiah(order.subtotal)}</TableCell>
             <TableCell className="hidden md:table-cell">
@@ -190,6 +203,80 @@ const OrdersPage = () => {
                 :
                 <TableRow>
                     <TableCell colSpan={6} className="text-center text-xl py-7">No orders found</TableCell>
+                </TableRow>
+            }
+            
+            </TableBody>
+        </Table>
+        </CardContent>
+            </Card>
+            <Card x-chunk="dashboard-06-chunk-0">
+                <CardHeader>
+                    <CardTitle>Cart</CardTitle>
+                    <CardDescription>
+                        Here are the list of items in your cart
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+        <Table>
+            <TableHeader>
+                            <TableRow>
+                                <TableHead>Product Name</TableHead>
+                                <TableHead>Quantity</TableHead>
+                                <TableHead>Subtotal</TableHead>
+                                <TableHead className="hidden md:table-cell">
+                                    Status
+                                </TableHead>
+                                <TableHead className="hidden md:table-cell">
+                                    Date
+                                </TableHead>
+                                <TableHead>
+                                    <span className="">Actions</span>
+                                </TableHead>
+                            </TableRow>
+                        </TableHeader>
+            <TableBody>
+            {
+                cart.length > 0 ?
+                cart.map(order => (
+                    <TableRow key={order.id}>
+            <TableCell className="font-medium">
+                {order.product_name}
+            </TableCell>
+            <TableCell className="font-medium">
+                {order.quantity}
+            </TableCell>
+            <TableCell>{toRupiah(order.subtotal)}</TableCell>
+            <TableCell className="hidden md:table-cell">
+                <Badge variant={order.status === "Paid" ? "default" : "secondary"}>{order.status}</Badge>
+            </TableCell>
+            <TableCell className="hidden md:table-cell">
+                {new Date(order.created_at).toLocaleString()}
+            </TableCell>
+            <TableCell>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button
+                            aria-haspopup="true"
+                            size="icon"
+                            variant="ghost"
+                        >
+                            <MoreHorizontal className="h-4 w-4" />
+                            <span className="sr-only">Toggle menu</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem onClick={() => pay(order.id)}>Pay</DropdownMenuItem>
+                        {/* <DropdownMenuItem>Cancel</DropdownMenuItem> */}
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </TableCell>
+        </TableRow>
+                ))
+                :
+                <TableRow>
+                    <TableCell colSpan={6} className="text-center text-xl py-7">No items in your cart   </TableCell>
                 </TableRow>
             }
             
